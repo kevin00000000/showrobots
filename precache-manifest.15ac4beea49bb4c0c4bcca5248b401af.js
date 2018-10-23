c8851f1f2a0605068125eb82b6fb6334ed6dc7cc
Kevin00000000 <jiannan_yang@163.com>
2018-10-23 10:11:28 +0800

    Updates

---
+++ precache-manifest.15ac4beea49bb4c0c4bcca5248b401af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/showrobots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "180a26a932b62ff69480",
+    "url": "/showrobots/static/js/runtime~main.180a26a9.js"
+  },
+  {
+    "revision": "2ca30deb4343d0244cee",
+    "url": "/showrobots/static/js/main.2ca30deb.chunk.js"
+  },
+  {
+    "revision": "09586ef98f33aacb0a3e",
+    "url": "/showrobots/static/js/1.09586ef9.chunk.js"
+  },
+  {
+    "revision": "2ca30deb4343d0244cee",
+    "url": "/showrobots/static/css/main.04d153cf.chunk.css"
+  },
+  {
+    "revision": "09586ef98f33aacb0a3e",
+    "url": "/showrobots/static/css/1.551d64c7.chunk.css"
+  },
+  {
+    "revision": "499feb0beeeea15f68a7c24ec747f2ee",
+    "url": "/showrobots/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/showrobots/precache-manifest.15ac4beea49bb4c0c4bcca5248b401af.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/showrobots/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
